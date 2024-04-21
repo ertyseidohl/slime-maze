95f9ea33c3a3b1e7633efb552749f303097760c2
Erty Seidohl <erty@seidohl.com>
2024-04-21 09:53:24 -0400

    add camera quads, background

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ local StateMenu = require 'statemenu'
 local StatePlaying = require 'stateplaying'
 
 local LAND_SPRITESHEET_PATH = "assets/Sprout Lands - Sprites - premium pack/Tilesets/ground tiles/New tiles/Grass_Hill_Tiles_v2.png"
+local GROUND_SPRITESHEET_PATH = "assets/Sprout Lands - Sprites - premium pack/Tilesets/ground tiles/New tiles/Soil_Ground_Tiles.png"
 
 local config = {
     mazeHeight = 12,
@@ -31,6 +32,21 @@ local function stateTransition(toState)
 end
 
 function love.load()
+    -- Ground tiles
+    spritesheets["ground"] = Spritesheet:new(GROUND_SPRITESHEET_PATH, config.tileSize, config.tileSize)
+    spritesheets["ground"]:nameQuads({
+        {"ground", 2, 2},
+        {"grounddark1", 1, 6},
+        {"grounddark2", 2, 6},
+        {"grounddark3", 3, 6},
+        {"grounddark4", 4, 6},
+        {"grounddark5", 5, 6},
+        {"groundlight1", 1, 7},
+        {"groundlight2", 2, 7},
+        {"groundlight3", 3, 7},
+        {"groundlight4", 4, 7},
+        {"groundlight5", 5, 7}
+    })
     -- Land tiles
     spritesheets["land"] = Spritesheet:new(LAND_SPRITESHEET_PATH, config.tileSize, config.tileSize)
     spritesheets["land"]:nameQuads({
--- stateplaying.lua
+++ stateplaying.lua
@@ -8,8 +8,57 @@ local StatePlaying = class('StatePlaying')
 local maze = nil
 local mazeCanvas = nil
 
+local GROUND_TILE_NAMES = {
+    "ground",
+    "grounddark1",
+    "grounddark2",
+    "grounddark3",
+    "grounddark4",
+    "grounddark5",
+    "groundlight1",
+    "groundlight2",
+    "groundlight3",
+    "groundlight4",
+    "groundlight5"
+}
+
+-- Cameras
+local quarterScreens = nil
+
 -- debug player
 local debugPlayer
+
+function CreateQuarterScreens()
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+
+    return {
+        {
+            x = 0,
+            y = 0,
+            width = screenWidth / 2,
+            height = screenHeight / 2
+        },
+        {
+            x = screenWidth / 2,
+            y = 0,
+            width = screenWidth / 2,
+            height = screenHeight / 2
+        },
+        {
+            x = 0,
+            y = screenHeight / 2,
+            width = screenWidth / 2,
+            height = screenHeight / 2
+        },
+        {
+            x = screenWidth / 2,
+            y = screenHeight / 2,
+            width = screenWidth / 2,
+            height = screenHeight / 2
+        }
+    }
+end
 
 function StatePlaying:createMaze()
     maze = Maze:new(self.config.mazeWidth, self.config.mazeHeight)
@@ -21,6 +70,19 @@ function StatePlaying:createMaze()
         self.config.mazeHeight * self.config.tileSize
     )
     love.graphics.setCanvas(mazeCanvas)
+    -- Draw ground
+
+    for y = 1, self.config.mazeHeight, 1 do
+        for x = 1, self.config.mazeWidth, 1 do
+            love.graphics.draw(
+                self.spritesheets["ground"].image, -- drawable
+                self.spritesheets["ground"]:getNamedQuad(GROUND_TILE_NAMES[math.random(#GROUND_TILE_NAMES)]), -- quad
+                self.config.tileSize * (x - 1), -- x, 1-based index to 0-based offet
+                self.config.tileSize * (y - 1) -- y, 1-based index to 0-based offet
+            )
+        end
+    end
+    -- Draw maze
     assert(maze ~= nil, "maze is nil!")
     for y = 1, self.config.mazeHeight, 1 do
         for x = 1, self.config.mazeWidth, 1 do
@@ -44,15 +106,14 @@ function StatePlaying:createMaze()
 end
 
 function StatePlaying:initialize(stateTransitionFunction, config, spritesheets)
-    print("Initialize state playing")
     self.stateTransitionFunction = stateTransitionFunction
     self.config = config
     self.spritesheets = spritesheets
 end
 
 function StatePlaying:enterState()
-    print("Enter state playing")
     self:createMaze()
+    self.quarterScreens = CreateQuarterScreens()
     debugPlayer = Player:new(1, 1, maze, self.config.tileSize * self.config.scaleFactor)
 end
 
@@ -72,6 +133,10 @@ function StatePlaying:update(dt)
 end
 
 function StatePlaying:draw(dt)
+    -- Draw the four quadrants
+    for _, rect in ipairs(self.quarterScreens) do
+        love.graphics.rectangle("line", rect.x, rect.y, rect.width, rect.height)
+    end
     -- Set premultiplied alpha blend mode since we've
     -- already rendered the maze to canvas
     love.graphics.setBlendMode("alpha", "premultiplied")
